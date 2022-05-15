a045787169d205b0bbc079d2edda29c293bf9ad8
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-05-15 20:58:38 +0300

    exe_14

---
+++ exe_14.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def combine_arrays(vector:int, array:np):
+    """
+    :param vector: Some vector.
+    :param array: Some array.
+    combining a one and a two-dimensional array together and displaying their elements.
+    """
+    for a, b in np.nditer([vector, array]):
+        print(a, " : ", b)
