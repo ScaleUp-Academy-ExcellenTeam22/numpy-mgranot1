54f410cc531fd355f5d314f81185397b183104bd
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-05-31 01:50:22 +0300

    try_2

--- exe_12.py
+++ exe_12.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def removing_single_dimensional_entries(array:np)->np:
+def removing_single_dimensional_entries(array:np.ndarray)->np.ndarray:
     """
 
     :param array: Some array.
