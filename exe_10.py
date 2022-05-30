2400351c26a374bec2e4c4cff3408692233667fb
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-05-31 01:48:38 +0300

    try_2

--- exe_10.py
+++ exe_10.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 
-def sort_axis(array:np, axis1:int)->np:
+def sort_axis(array: np.ndarray, axis1: int) -> np.ndarray:
     """
     :param array: Some array
     :param axis1: Sets the sort.
     :return: array who sorted along the first, last axis of an array.
     """
-    print(np.sort(array, axis=axis1))
     return np.sort(array, axis=axis1)
 
 
