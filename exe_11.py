a5a596a6551bc690e853cafb3cb51629710323d7
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-05-31 01:49:33 +0300

    try_2

--- exe_11.py
+++ exe_11.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def diagonal(size:int)->np:
+def diagonal(size:int)-> np.ndarray:
     """
     :param size: Size of array.
     :return: A array with ones on a diagonal and zeros elsewhere.
