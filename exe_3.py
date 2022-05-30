b381cad8c4579221151e319ce33609d149367d83
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-05-31 01:19:53 +0300

    try_2

--- exe_3.py
+++ exe_3.py
@@ -3,11 +3,9 @@ import numpy as np
 
 def number_of_rows_and_columns(matrix:np)->tuple:
     """
-    :param matrix: Two-dimensional matrix.
+    :param matrix: A given matrix.
     :return: the number of rows and columns of the matrix.
     """
-    rows = len(matrix[:])
-    collumn = len(matrix[:][0])
-    return (rows, collumn)
+    return matrix.shape
 
 
