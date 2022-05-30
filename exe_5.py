95f505ef09fdf443da2dfb44978462f5f3a691e1
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-05-31 01:36:53 +0300

    try_2

--- exe_5.py
+++ exe_5.py
@@ -1,18 +1,12 @@
 import numpy as np
 
 
-def add_vector_to_matrix(vector:np, matrix:np)->np:
+def add_vector_to_matrix(vector: np.array, matrix: np.ndarray) -> np.ndarray:
     """
     :param vector: Vector to add to all columns in the matrix.
     :param matrix: Some Matrix.
     :return: matrix with the addition of a vector to each row of the matrix.
     """
-    return np.array([matrix[i, :] + vector for i in range(4)])
+    return matrix[:, np.newaxis] + vector
 
 
-"""
-m = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
-v = np.array([1, 1, 0])
-
-print(add_vector_to_matrix(v,m))
-"""
